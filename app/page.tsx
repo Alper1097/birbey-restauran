231be34ace89609e41820ad07de0b8df8de67f59
Alper1097 <alperonal445@gmail.com>
2026-08-08 03:55:37 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,28 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Catering from "@/components/Catering";
+import Menu from "@/components/Menu";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+
       <Hero />
+
       <About />
+
       <Services />
+
       <Catering />
+
+      <Menu />
+
+      <Contact />
+
+      <Footer />
     </main>
   );
 }
